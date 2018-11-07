fe0752971a10eb7e2f7f94d2f48c371815e4ea3c
shakib500 <shakib.nazmus@northsouth.edu>
2018-11-07 21:48:20 +0600

    postgresql connected

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': '01710265961s',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
