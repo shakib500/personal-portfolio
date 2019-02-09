ee4e62f0327289cc83e10b1a736238c225c61429
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-02-09 19:44:36 +0600

    update

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import contact
 
 def home(request):
     if request.method == 'POST':
@@ -7,4 +8,11 @@ def home(request):
         email = request.POST.get("email")
         subject = request.POST.get("subject")
         message = request.POST.get("message")
+
+        contact = contact()
+        contact.name = name
+        contact.email = email
+        contact.message = message
+        contact.save()
+
     return render(request,'home.html')
