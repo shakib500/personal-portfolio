a32fa447c3bff34eda3832731de1dda82cd71d5e
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-02-09 19:40:46 +0600

    created views in contact apps

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        subject = request.POST.get("subject")
+        message = request.POST.get("message")
+    return render(request,'home.html')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+import contact.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', contact.views.home, name='home'),
 ]
