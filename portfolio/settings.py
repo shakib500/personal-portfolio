c1bbc0782d1ab6e40bea9e3d0a7abb205cea823a
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-02-14 20:32:49 +0600

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'kh7#*qc=8&mnwefzxlb4+ejo+3cgg_iodbpddi-cl=d@pj*!tu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kingdomofnoway.herokuapp.com','www.kingdomofnoway.ga']
 
 
 # Application definition
